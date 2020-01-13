8c6cf224d6802eb3977fa48354165d83ebcb5543
Tyson Cruz <32321720+TMCRUZ24@users.noreply.github.com>
2020-01-12 19:12:37 -0800

    Add files via upload

---
+++ Assignment1/HelloWorld.c
@@ -0,0 +1,16 @@
+/*
+Tyson M. Cruz
+01/12/2020
+CST221 - Operating Systems Concepts
+Professor Michael Landreth
+
+Hello World
+*/
+
+
+#include<stdio.h>
+int main()
+{
+	printf("Hello World - By Tyson Cruz\n");
+	return 0;
+}
